37ee0e6c13f6f8ef4f324a062a555ac51a30269c
hemuprabu <hemalathaprakasam219@gmail.com>
2024-05-07 18:13:16 -0400

    date formatted

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,47 +1,52 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
-const dayjs = require('dayjs');
+const sequelize = require("../config/connection");
+const dayjs = require("dayjs");
 class BlogPost extends Model {}
 
-
- BlogPost.init( {
-    id:{
-        type: DataTypes.INTEGER,
+BlogPost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
     title: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     content: {
-        type: DataTypes.TEXT,
-        allowNull: false
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
     author: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    createdAt:{
-        type: DataTypes.DATE,
+      type: DataTypes.STRING,
       allowNull: false,
-    defaultValue:dayjs().format('MM-DD-YYYY')
-    }
-},
-{
-    hooks: {
-        beforeCreate: async (blogPost, options) => {
-            blogPost.createdAt = dayjs().format('YYYY-MM-DD HH:mm:ss');
-          },
+    },
+    createdAt: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: function () {
+        const currentDate = new Date();
+        const formattedDate =
+          currentDate.getMonth() +
+          1 +
+          "-" +
+          currentDate.getDate() +
+          "-" +
+          currentDate.getFullYear();
+        return formattedDate;
+      },
+    },
   },
-    
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'blogPost',
-  });
+    modelName: "blogPost",
+  }
+);
 
 module.exports = BlogPost;
